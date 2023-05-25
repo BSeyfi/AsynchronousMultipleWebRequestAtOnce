a9ea1faf8497a2443aa138cfaf5a1891b22c9402
BSeyfi <42473071+BSeyfi@users.noreply.github.com>
2023-05-26 00:53:59 +0330

    Initil commit

---
+++ Asynchronous/AsyncSandbox.cs
@@ -0,0 +1,22 @@
+﻿namespace Asynchronous;
+
+public class AsyncSandbox
+{
+    private const string _mockDelayWebApiURL = "http://mockbin.org/delay/";
+
+    public static async Task<string?> HttpGetWithDelay(int delayInMiliSeconds)
+    {
+        using (var client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"{_mockDelayWebApiURL}{delayInMiliSeconds}");
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Asynchronous/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Asynchronous;
+
+class Program
+{
+    
+    public async static Task Main(string[] args)
+    {
+        int delay = 500;
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+
+        //Create an array of tasks
+        Task<string?>[] tasks = new Task<string?>[10];
+        string?[] tasksResult = new string?[10];
+        foreach (var index in Enumerable.Range(0,10))
+        {
+            tasks[index] = AsyncSandbox.HttpGetWithDelay(delay);
+        }
+        
+        // Wait for all tasks to be done
+        tasksResult= await Task.WhenAll(tasks);
+        
+        
+        stopwatch.Stop();
+
+        foreach (var index in Enumerable.Range(0,10))
+            
+        {
+            Console.WriteLine($"Task {index} response message is:\n{tasksResult[index]}\n");
+        }
+
+        Console.WriteLine($"Elapsed Time For Completion of 10 Get Requests each wait at least {delay} miliseconds on serevr side: {stopwatch.ElapsedMilliseconds} milisecond");
+    }
+
+    public async static Task<string> SampleAsyncMethod()
+    {
+        return "";
+    }
+}
